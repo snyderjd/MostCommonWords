f4fb3ec61e13ccaff8eafb67ded6a18a2da21015
snyder <snyderjosephd@yahoo.com>
2019-10-14 14:31:05 -0500

    Completed part 1 - print out top 10 occurring words

--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Net;
+using System.Linq;
+using System.Collections.Generic;
 
 namespace MostCommonWords
 {
@@ -6,7 +9,61 @@ namespace MostCommonWords
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // WebClient client = new WebClient();
+            // string nightOceanWords = client.DownloadString("http://www.hplovecraft.com/writings/texts/fiction/no.aspx");
+            // Console.WriteLine(nightOceanWords);
+            NightOcean nightOcean = new NightOcean();
+
+            string lowerCaseStory = nightOcean.Story.ToLower();
+
+            // char[] goodChars = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p',
+            //      'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', ' '};"
+
+            List<char> goodChars = new List<char>(){
+                'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p',
+                 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', ' '
+            };
+
+            // Create a new string with only the letters and spaces
+            string wordsOnly = "";
+
+            foreach(char character in lowerCaseStory)
+            {
+                if (goodChars.Contains(character))
+                {
+                    wordsOnly += character;
+                }
+            }
+
+            // Console.WriteLine(wordsOnly);
+
+            // Split the wordsOnly string on a space to get a list of strings
+
+            string[] allWords = wordsOnly.Split(" ");
+
+            foreach(string word in allWords)
+            {
+                // Console.WriteLine(word);
+            }
+
+            var wordCount = allWords.GroupBy(word => word, (key, value) => new WordCount 
+                {
+                    Word = key,
+                    Count = value.Count()
+                }).OrderByDescending(result => result.Count).ToList();
+
+            foreach(WordCount item in wordCount)
+            {
+                Console.WriteLine($"{item.Word}: {item.Count}");
+            }
+
+            // Print out the top ten words
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine($"{wordCount[i].Word}: {wordCount[i].Count}");
+            }
+
+
         }
     }
 }
---
+++ WordCount.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace MostCommonWords
+{
+    public class WordCount
+    {
+        public string Word { get; set; }
+        public int Count { get; set; }
+    }
+}
